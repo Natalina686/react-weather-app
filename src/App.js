d03e392412c39a78204da9a3e5bfdf72a96c173a
Nevada686 <natalina686@gmail.com>
2022-12-18 21:20:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
+      <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
         <a href="" target="_blank">
